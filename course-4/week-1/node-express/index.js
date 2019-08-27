d5e3eb7da0f76cbbdbad8885ffb832dea45cc40d
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2019-08-27 15:12:43 +0530

    using Express to Serve Static Files

--- course-4/week-1/node-express/index.js
+++ course-4/week-1/node-express/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = "3000";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
   console.log(req.headers);
@@ -12,6 +16,7 @@ app.use((req, res, next) => {
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
+
 
 const server = http.createServer(app);
 
