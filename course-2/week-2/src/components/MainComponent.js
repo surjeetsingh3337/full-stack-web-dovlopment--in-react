edd60ca5019b15667799fcd5df02d8e864e98c1f
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-01-09 18:51:17 +0530

    demonstrated Header and Footer of course-2/week-2

--- course-2/week-2/src/components/MainComponent.js
+++ course-2/week-2/src/components/MainComponent.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+
 import Menu from "./MenuComponent";
 import DishDetail from "./DishDetailComponent";
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+
 import { DISHES } from "../shared/dishes";
 
 class Main extends Component {
@@ -20,11 +23,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header/>
         <Menu
           dishes={this.state.dishes}
           onDishSelect={dishId => this.onDishSelect(dishId)}
@@ -36,6 +35,7 @@ class Main extends Component {
             )[0]
           }
         />
+        <Footer/>
       </div>
     );
   }
