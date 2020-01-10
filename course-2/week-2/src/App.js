702bb77b48d998af174ea0bec1fb171c942992eb
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-01-10 12:44:21 +0530

    demonstrated Intro. to Redux of course-2/week-3

--- course-2/week-2/src/App.js
+++ course-2/week-2/src/App.js
@@ -4,6 +4,11 @@ import "./App.css";
 import Main from "./components/MainComponent";
 import { DISHES } from "./shared/dishes";
 
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+
+const store = ConfigureStore();
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -15,11 +20,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <div className="App">
-            <Main />
-          </div>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <div className="App">
+              <Main />
+            </div>
+          </BrowserRouter>
+        </Provider>
       </div>
     );
   }
