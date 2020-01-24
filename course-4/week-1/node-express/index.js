368fae5e897b4e78769c6b9bceb74da956cbe8dc
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-01-24 13:33:09 +0530

    fixed file name

--- course-4/week-1/node-express/index.js
+++ course-4/week-1/node-express/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const http = require("http");
-const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
-const dishRoutes = require("./routes/dishRoutes");
+const dishRouter = require("./routes/dishRouter");
 const promoRouter = require("./routes/promoRouter");
 const leaderRouter = require("./routes/leaderRouter");
 
@@ -14,7 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/dishes", dishRoutes);
+app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 
