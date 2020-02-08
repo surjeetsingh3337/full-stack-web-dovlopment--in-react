4050a92f86d2fa0677999937d908062186d1976b
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-02-08 15:55:03 +0530

    demonstrated 'Mongoose Population' from course-4/week-3

--- course-4/week-3/conFusionServer/models/dishes.js
+++ course-4/week-3/conFusionServer/models/dishes.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 require("mongoose-currency").loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
-const commentSchema = new Schema(
+var commentSchema = new Schema(
   {
     rating: {
       type: Number,
@@ -17,8 +17,8 @@ const commentSchema = new Schema(
       required: true
     },
     author: {
-      type: String,
-      required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
     }
   },
   {
--- course-4/week-3/conFusionServer/routes/dishRouter.js
+++ course-4/week-3/conFusionServer/routes/dishRouter.js
@@ -12,6 +12,7 @@ dishRouter
   .route("/")
   .get((req, res, next) => {
     Dishes.find({})
+      .populate("comments.author")
       .then(
         dishes => {
           res.statusCode = 200;
@@ -56,6 +57,7 @@ dishRouter
   .route("/:dishId")
   .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
+      .populate("comments.author")
       .then(
         dish => {
           res.statusCode = 200;
@@ -105,6 +107,7 @@ dishRouter
   .route("/:dishId/comments")
   .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
+      .populate("comments.author")
       .then(
         dish => {
           if (dish != null) {
@@ -126,12 +129,17 @@ dishRouter
       .then(
         dish => {
           if (dish != null) {
+            req.body.author = req.user._id;
             dish.comments.push(req.body);
             dish.save().then(
               dish => {
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "application/json");
-                res.json(dish);
+                Dishes.findById(dish._id)
+                  .populate("comments.author")
+                  .then(dish => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(dish);
+                  });
               },
               err => next(err)
             );
@@ -184,6 +192,7 @@ dishRouter
   .route("/:dishId/comments/:commentId")
   .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
+      .populate("comments.author")
       .then(
         dish => {
           if (dish != null && dish.comments.id(req.params.commentId) != null) {
@@ -226,9 +235,13 @@ dishRouter
             }
             dish.save().then(
               dish => {
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "application/json");
-                res.json(dish);
+                Dishes.findById(dish._id)
+                  .populate("comments.author")
+                  .then(dish => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(dish);
+                  });
               },
               err => next(err)
             );
@@ -254,9 +267,13 @@ dishRouter
             dish.comments.id(req.params.commentId).remove();
             dish.save().then(
               dish => {
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "application/json");
-                res.json(dish);
+                Dishes.findById(dish._id)
+                  .populate("comments.author")
+                  .then(dish => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(dish);
+                  });
               },
               err => next(err)
             );
