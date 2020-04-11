70506457586cc5b4eab3be13127257fcc2a346c8
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-04-11 09:50:07 +0530

    sparkles: feat: demonstrate Components Part 1 in course3/week1

--- course-3/week-1/confusion/App.js
+++ course-3/week-1/confusion/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Main from "./components/MainComponent";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return <Main />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
