34ef54c8019039af70ce4a477bfcc5addcee82f3
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-04-15 20:05:16 +0530

    sparkles: feat: demonstrate 'Modal' in course3/week2

--- course-3/week-2/confusion/components/ReservationComponent.js
+++ course-3/week-2/confusion/components/ReservationComponent.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import {
   Text,
   View,
-  ScrollView,
   StyleSheet,
   Picker,
   Switch,
   Button,
+  Modal,
+  ScrollView,
 } from "react-native";
+
 import { Card } from "react-native-elements";
 import DatePicker from "react-native-datepicker";
 
@@ -19,6 +21,7 @@ class Reservation extends Component {
       guests: 1,
       smoking: false,
       date: "",
+      showModal: false,
     };
   }
 
@@ -26,12 +29,21 @@ class Reservation extends Component {
     title: "Reserve Table",
   };
 
+  toggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+
   handleReservation() {
     console.log(JSON.stringify(this.state));
+    this.toggleModal();
+  }
+
+  resetForm() {
     this.setState({
       guests: 1,
       smoking: false,
       date: "",
+      showModal: false,
     });
   }
 
@@ -100,6 +112,35 @@ class Reservation extends Component {
             accessibilityLabel="Learn more about this purple button"
           />
         </View>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.showModal}
+          onDismiss={() => this.toggleModal()}
+          onRequestClose={() => this.toggleModal()}
+        >
+          <View style={styles.modal}>
+            <Text style={styles.modalTitle}>Your Reservation</Text>
+            <Text style={styles.modalText}>
+              Number of Guests: {this.state.guests}
+            </Text>
+            <Text style={styles.modalText}>
+              Smoking?: {this.state.smoking ? "Yes" : "No"}
+            </Text>
+            <Text style={styles.modalText}>
+              Date and Time: {this.state.date}
+            </Text>
+
+            <Button
+              onPress={() => {
+                this.toggleModal();
+                this.resetForm();
+              }}
+              color="#512DA8"
+              title="Close"
+            />
+          </View>
+        </Modal>
       </ScrollView>
     );
   }
@@ -119,6 +160,22 @@ const styles = StyleSheet.create({
   },
   formItem: {
     flex: 1,
+  },
+  modal: {
+    justifyContent: "center",
+    margin: 20,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    backgroundColor: "#512DA8",
+    textAlign: "center",
+    color: "white",
+    marginBottom: 20,
+  },
+  modalText: {
+    fontSize: 18,
+    margin: 10,
   },
 });
 
